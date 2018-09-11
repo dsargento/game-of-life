4f5732c72f5dd06adaede971aa5cb27eed1826cb
David Sargento <david.sargento10@gmail.com>
2018-09-11 14:49:27 +0200

    first commit

---
+++ game-of-life.py
@@ -0,0 +1,28 @@
+from sys import argv
+import urwid
+
+import os
+
+
+def main():
+    if len(argv) <= 1:
+        print("Usage: python3.6 game-of-life.py [input file path]")
+        return
+    try:
+        file = open(argv[1], 'r', 1)
+    except IOError:
+        print("File not found or path is incorrect")
+        return
+    array = build_array(file)
+    print(array)
+
+
+def build_array(file):
+    array = []
+    for line in file:
+        array.append(line)
+    return array
+
+
+if __name__ == '__main__':
+    main()
